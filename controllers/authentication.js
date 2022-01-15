7fdba01e58d1b03a8971f39984535166ba448b77
Harsha Prajapati <harsha.prajapati@scripbox.com>
2022-01-15 16:04:53 +0530

    Logic to see if user already exist

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,13 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    // res.send({'sucess': 'true'})
+    //to pull out data from req object
+    const email = req.body.email;
+    const password = req.body.password;
     //see if the user with a given email and passwrod exist
+    User.findOne({email: email}, function(err, exisitingUser) {
+        
+    })
     //If it does exist return an error
     //if not exist aand  a frsh email thern create and save user record
 
