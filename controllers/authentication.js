98f0f0f21d348d68a63848037cef1a37250cf908
Harsha Prajapati <harsha.prajapati@scripbox.com>
2022-01-15 16:13:43 +0530

    Creating user records

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,10 +6,28 @@ exports.signup = function(req, res, next) {
     const password = req.body.password;
     //see if the user with a given email and passwrod exist
     User.findOne({email: email}, function(err, exisitingUser) {
-        
-    })
-    //If it does exist return an error
-    //if not exist aand  a frsh email thern create and save user record
+        if(err) { return next(err)}
 
-    //Respond to request user was created
+        //If email already does exist return an error
+        if(exisitingUser) {
+            return res.status(422).send({ err: 'Email already in use'})
+        }
+
+        //if not exist aand  a frsh email thern create and save user record
+        const newUser = new User({
+            email: email,
+            password: password
+        })
+
+        newUser.save(function(err) {
+            if(err) {
+                return next(err);
+            }
+            res.json(newUser);
+        }); //talking to db to save it there
+
+        //Respond to request user was created
+    })
+
+
 }
