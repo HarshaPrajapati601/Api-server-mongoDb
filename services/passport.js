41db7d99e2c1e3042fc2909b7b28ece6c8004778
Harsha Prajapati <harsha.prajapati@scripbox.com>
2022-01-16 20:10:04 +0530

    Setup options for JWT stratergy

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,16 @@ const JwtStratergy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 //setup options for JWT stratergy
-const jwtStratergy = {};
+const jwtStratergy = {
+    //to tell the JwtStratergy to where to look for the key
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+
+};
+
+//1. that whenever we request comes in and we want passport to handle it it needs to look at the request
+// header and specifically a header called authorization to find the token.
+//2.secret to decode the token
 
 //Create JWT stratergy
 const jwtLogin = new JwtStratergy(jwtStratergy, function(payload, done) {
@@ -29,3 +38,4 @@ const jwtLogin = new JwtStratergy(jwtStratergy, function(payload, done) {
 
 
 //Tell passport to use this stratergy
+passport.use(jwtLogin);
